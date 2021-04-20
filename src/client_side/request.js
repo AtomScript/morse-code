04759f06adeafd36de867c719604dd8bd6ecac73
Rafael <77973453+AtomScript@users.noreply.github.com>
2021-04-20 15:51:22 -0400

    Update request.js

--- src/client_side/request.js
+++ src/client_side/request.js
@@ -18,12 +18,8 @@ let decodeText = document.getElementById("resultText");
         </tr>`;
        
      for(let res of data.arr){
-       table.innerHTML += "<tr><td>"+res.letter+"</td><td>"+res.code+"</td></tr>"
+       table.innerHTML += "<tr class='linesTable'><td class='colum'>"+res.letter+"</td><td class='colum'>"+res.code+"</td></tr>"
      }
-},
-error: function(xhr, status, error) {
-  var err = eval("(" + xhr.responseText + ")");
-  alert(err.Message);
 }
 });
 }catch(err){console.log(err)};
@@ -49,12 +45,8 @@ let decodeText = document.getElementById("morseText");
         </tr>`;
        
      for(let res of data.arr){
-       table.innerHTML += "<tr><td>"+res.letter+"</td><td>"+res.code+"</td></tr>"
+       table.innerHTML += "<tr class='linesTable'><td class='colum'>"+res.letter+"</td><td class='colum'>"+res.code+"</td></tr>"
      }
-},
-error: function(xhr, status, error) {
-  var err = eval("(" + xhr.responseText + ")");
-  alert(err.Message);
 }
 });
 }catch(err){console.log(err)};
@@ -80,98 +72,58 @@ function showModalMorse() {
   document.getElementById("gm").style.display = "block";
 }
 
-function hidegt(){
+function hidegt() {
   document.getElementById("gt").style.display = "none";
 }
 
-function hidegm(){
+function hidegm() {
   document.getElementById("gm").style.display = "none";
 }
-function encode() {
+
+function colorText() {
   try{
-    let encodeText = document.getElementById("texter");
-let decodeText = document.getElementById("resultText");
-
-     $.ajax({
-  url: '/encode',
-  method: 'POST',
-  data: {text: encodeText.value},
-   success: function(data){
-     decodeText.innerHTML = data.text;
-     const table = document.getElementById("tableText");
-       
-       table.innerHTML = `<tr>
-          <th>Character</th>
-          <th>Code</th>
-        </tr>`;
-       
-     for(let res of data.arr){
-       table.innerHTML += "<tr><td>"+res.letter+"</td><td>"+res.code+"</td></tr>"
-     }
-},
-error: function(xhr, status, error) {
-  var err = eval("(" + xhr.responseText + ")");
-  alert(err.Message);
-}
-});
-}catch(err){console.log(err)};
+  const table = document.getElementById("tableText");
+  let lines = table.getElementsByClassName("linesTable");
+  
+  for(let line of lines){
+    line.style.color = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+  }
+  
+  }catch{}
 }
 
-
-function decode() {
+function colorMorse() {
   try{
-    let encodeText = document.getElementById("resultMorse");
-let decodeText = document.getElementById("morseText");
-
-     $.ajax({
-  url: '/decode',
-  method: 'POST',
-  data: {text: decodeText.value},
-   success: function(data){
-     encodeText.innerHTML = data.text;
-     const table = document.getElementById("tableMorse");
-       
-       table.innerHTML = `<tr>
-          <th>Code</th>
-          <th>Character</th>
-        </tr>`;
-       
-     for(let res of data.arr){
-       table.innerHTML += "<tr><td>"+res.letter+"</td><td>"+res.code+"</td></tr>"
-     }
-},
-error: function(xhr, status, error) {
-  var err = eval("(" + xhr.responseText + ")");
-  alert(err.Message);
-}
-});
-}catch(err){console.log(err)};
+  const table = document.getElementById("tableMorse");
+  let lines = table.getElementsByClassName("linesTable");
+  
+  for(let line of lines){
+    line.style.color = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+  }
+  
+  }catch{}
 }
 
-function changeToMorse() {
-  document.getElementById("morseDiv").style.display = "block";
-  document.getElementById("textDiv").style.display = "none";
+function returnColorText() {
+  try{
+  const table = document.getElementById("tableText");
+  let lines = table.getElementsByClassName("linesTable");
+  
+  for(let line of lines){
+    line.style.color = "#000000";
+  }
+  
+  }catch{}
 }
 
-function changeToText() {
-  document.getElementById("textDiv").style.display = "block";
-  document.getElementById("morseDiv").style.display = "none";
-}
-
-function showModalText() {
-  document.getElementById("gt").style.display = "block";
-  document.getElementById("gm").style.display = "none";
-}
-
-function showModalMorse() {
-  document.getElementById("gt").style.display = "none";
-  document.getElementById("gm").style.display = "block";
-}
-
-function hidegt(){
-  document.getElementById("gt").style.display = "none";
-}
-
-function hidegm(){
-  document.getElementById("gm").style.display = "none";
+function returnColorMorse() {
+  try{
+  const table = document.getElementById("tableText");
+  let lines = table.getElementsByClassName("linesTable");
+  
+  for(let line of lines){
+    line.style.color = "#000000";
+  }
+  
+  }catch{}
 }
